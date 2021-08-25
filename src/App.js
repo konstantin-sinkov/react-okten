957b39e1423e2fecdec44b263f37ba57b78b35a3
konstantin-sinkov <sinkov.ks@gmail.com>
2021-08-25 22:53:58 +0300

    lesson_7-1 | start

--- src/App.js
+++ src/App.js
@@ -4,26 +4,19 @@ import Users from "./components/users/Users";
 import {Route} from "react-router";
 import Posts from "./components/posts/Posts";
 
-// Є дві лінки
-// /users
-// /posts
+// є об'єкт, значення якого 0
+// Стоврити 2 кнопки.
+//     Перша збільшує   значення на 10
+// Друга зменшує на 2.
 //
-// /users - робить все те, що було на занятті останнім способом (через кнопку та ддатковий запит на сервер)
-// /posts - Отримує posts з jsobplaceholder, виводить їх всі.
-// Біля кожного поста зробити лінку (не конопку) яка буде вести на детальну інформацію поста. Детальну інформацію
-// отримувати через history.state
+// Зміну стану та виведення реалізувати через redcer
 
 
 function App() {
   return (
-      <Router>
-          <div className={'nav_bar'}>
-            <Link to={'/users'}>Users</Link>
-            <Link to={'/posts'}>Posts</Link>
-          </div>
-          <Route path={'/users'} component={Users} />
-          <Route path={'/posts'} component={Posts} />
-      </Router>
+     <div>
+
+     </div>
 
   );
 }
